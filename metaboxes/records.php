d66e581b45c1579c5bc7b16db11ec713ed0ce51f
Luke McDonald <thelukemcdonald@gmail.com>
2012-04-09 00:52:34 -0500

    Added I18n to text strings

--- metaboxes/records.php
+++ metaboxes/records.php
@@ -4,15 +4,17 @@
  *
  * @since 1.0
  */
-function audiotheme_add_record_meta(){
+function audiotheme_add_record_meta() {
+	
     add_meta_box( 
         'audiotheme-record-meta', 
-        'Record Details', 
+        __( 'Record Details', 'audiotheme' ), 
         'audiotheme_record_meta_cb', 
         'audiotheme_record', 
         'normal', 
         'high'
     );
+    
 }
 add_action( 'add_meta_boxes', 'audiotheme_add_record_meta' );
 
@@ -21,10 +23,11 @@ add_action( 'add_meta_boxes', 'audiotheme_add_record_meta' );
  *
  * @since 1.0
  */
-function audiotheme_record_meta_cb( $post ){
-    /* Nonce to verify intention later */
+function audiotheme_record_meta_cb( $post ) {
+	
+	// Nonce to verify intention later
 	wp_nonce_field( 'save_audiotheme_record_meta', 'audiotheme_record_nonce' );
-	audiotheme_meta_field( $post, 'text', '_tracks', 'Tracks', 'For development. Comma separated list of track ID\'s' );
+	audiotheme_meta_field( $post, 'text', '_tracks', __( 'Tracks', 'audiotheme' ), __( 'For development. Comma separated list of track ID\'s', 'audiotheme' ) );
 }
 
 /**
@@ -33,17 +36,18 @@ function audiotheme_record_meta_cb( $post ){
  * @since 1.0
  */
 function audiotheme_record_save( $post_id ) {
-    // Let's not auto save the data
+	
+	// Let's not auto save the data
 	if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return; 
 
 	// Check our nonce
-	if( !isset( $_POST['audiotheme_record_nonce'] ) || !wp_verify_nonce( $_POST['audiotheme_record_nonce'], 'save_audiotheme_record_meta' ) ) return;
+	if( ! isset( $_POST['audiotheme_record_nonce'] ) || ! wp_verify_nonce( $_POST['audiotheme_record_nonce'], 'save_audiotheme_record_meta' ) ) return;
 
 	// Make sure the current user can edit the post
-	if( !current_user_can( 'edit_post' ) ) return;
+	if( ! current_user_can( 'edit_post' ) ) return;
 	
-    // Save metadata
-    audiotheme_update_post_meta( $post_id, array('_tracks'), 'text' );
+	// Save metadata
+	audiotheme_update_post_meta( $post_id, array( '_tracks' ), 'text' );
 
 }
 add_action( 'save_post', 'audiotheme_record_save' );
