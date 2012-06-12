c7ad59b784cdfcc4ba7bfcdfcaf0237a0b12ace8
Luke McDonald <thelukemcdonald@gmail.com>
2012-06-12 16:02:38 -0500

    Update record link sources. Add functions to check and get track file url.

--- discography/general-template.php
+++ discography/general-template.php
@@ -1,14 +1,35 @@
 <?php
+/**
+ * Record Link Sources
+ *
+ * List of default resources in which records can be purchased.
+ * The options listed here show up as suggestions when the user types.
+ *
+ * @return array
+ * @since 1.0
+ */
 function get_audiotheme_record_link_sources() {
 	$default_sources = array(
+		'7digital'   => array( 'icon' => '' ),
 		'Amazon MP3' => array( 'icon' => '' ),
-		'CD Baby' => array( 'icon' => '' ),
-		'iTunes' => array( 'icon' => '' )
+		'Bandcamp'   => array( 'icon' => '' ),
+		'CD Baby'    => array( 'icon' => '' ),
+		'Google'     => array( 'icon' => '' ),
+		'iTunes'     => array( 'icon' => '' )
 	);
 	
 	return apply_filters( 'audiotheme_record_link_sources', $default_sources );
 }
 
+
+/**
+ * Record Type Strings
+ *
+ * List of default record types to better define the record, much like a post format.
+ *
+ * @return array
+ * @since 1.0
+ */
 function get_audiotheme_record_type_strings() {
 	$strings = array(
 		'record-type-album'  => _x( 'Album',  'Record type', 'audiotheme-i18n' ),
@@ -17,31 +38,41 @@ function get_audiotheme_record_type_strings() {
 	return $strings;
 }
 
+
+/**
+ * Get Record Type Slugs
+ *
+ * Gets and sets an array of available record type slugs from record type strings.
+ *
+ * @return array
+ * @since 1.0
+ */
 function get_audiotheme_record_type_slugs() {
 	$slugs = array_keys( get_audiotheme_record_type_strings() );
 	return array_combine( $slugs, $slugs );
 }
 
+
+/**
+ * Get Record Type String
+ *
+ * Sets default value of record type if option is not set.
+ *
+ * @return array
+ * @since 1.0
+ */
 function get_audiotheme_record_type_string( $slug ) {
+	
 	$strings = get_audiotheme_record_type_slugs();
+	
 	if ( ! $slug ) {
 		return $strings['record-type-album'];
 	} else {
 		return ( isset( $strings[ $slug ] ) ) ? $strings[ $slug ] : '';
 	}
+	
 }
 
-function audiotheme_track_has_download( $post_id ) {
-	$return = false;
-	
-	$allow_download = get_post_meta( $post_id, '_allow_download', true );
-	if ( $allow_download ) {
-		$file_url = get_post_meta( $post_id, '_file_url', true );
-		if ( $file_url && false === strpos( $file_url, 'spotify:' ) ) {
-			$return = $file_url;
-		}
-	}
-	
-	return apply_filters( 'audiotheme_track_download_url', $return, $post_id );
-}
+
+
 ?>
