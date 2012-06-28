a2134ef379a0423fc38f3dd92833f586f014e450
Brady Vercher <brady@blazersix.com>
2012-06-27 20:41:46 -0700

    Updated the_audiotheme_post_video() to match get_the_audiotheme_post_video().

--- videos/post-video-template.php
+++ videos/post-video-template.php
@@ -31,11 +31,11 @@ function get_audiotheme_post_video_url( $post_id = null ) {
  *
  * @since 1.0.0
  *
- * @param array $size Optional. Video size.  Defaults to 640 width.
- * @param string|array $attr Optional. Query string or array of attributes.
+ * @param array $args Optional. (width, height, discover)
+ * @param array $query_args Optional. Provider specific parameters.
  */
-function the_audiotheme_post_video( $size = array( 'width' => 640 ), $attr = '' ) {
-	echo get_the_audiotheme_post_video( null, $size, $attr );
+function the_audiotheme_post_video( $args = array(), $query_args = array() ) {
+	echo get_the_audiotheme_post_video( get_the_ID(), $args, $query_args );
 }
 
 
@@ -47,24 +47,21 @@ function the_audiotheme_post_video( $size = array( 'width' => 640 ), $attr = '' 
  * @param int $post_id Optional. Post ID.
  * @param array $args Optional. (width, height, discover)
  * @param array $query_args Optional. Provider specific parameters.
- * @param string|array $attr Optional. Query string or array of attributes.
  */
 function get_the_audiotheme_post_video( $post_id = null, $args = array(), $query_args = array() ) {
-	
 	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 	$video_url = get_audiotheme_post_video_url( $post_id );
 	
-	$args = wp_parse_args( $args, array( 'discover' => apply_filters( 'embed_oembed_discover', false ) && author_can( $post_id, 'unfiltered_html' ) )
-	);
+	$args = wp_parse_args( $args, array(
+		'discover' => apply_filters( 'embed_oembed_discover', false ) && author_can( $post_id, 'unfiltered_html' )
+	) );
 	
 	$html = '';
-	
 	if ( $video_url ) {
 		$html = wp_oembed_get( add_query_arg( $query_args, $video_url ), $args );
 	}
 	
 	return apply_filters( 'audiotheme_post_video_html', $html, $post_id, $video_url, $args, $query_args );
-
 }
 
 ?>
