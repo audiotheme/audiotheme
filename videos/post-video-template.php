f77689e3bee1dfe358c9c32627b5cb958ec7026a
Brady Vercher <brady@blazersix.com>
2012-06-28 22:37:49 -0700

    Changed get_the_audiotheme_post_video() to run the call WP_Embed->shortcode() for caching purposes and to take advantage of any registered handlers. Ugly, but without an better API, it does the job.

--- videos/post-video-template.php
+++ videos/post-video-template.php
@@ -31,7 +31,7 @@ function get_audiotheme_post_video_url( $post_id = null ) {
  *
  * @since 1.0.0
  *
- * @param array $args Optional. (width, height, discover)
+ * @param array $args Optional. (width, height)
  * @param array $query_args Optional. Provider specific parameters.
  */
 function the_audiotheme_post_video( $args = array(), $query_args = array() ) {
@@ -45,20 +45,33 @@ function the_audiotheme_post_video( $args = array(), $query_args = array() ) {
  * @since 1.0.0
  *
  * @param int $post_id Optional. Post ID.
- * @param array $args Optional. (width, height, discover)
+ * @param array $args Optional. (width, height)
  * @param array $query_args Optional. Provider specific parameters.
  */
 function get_the_audiotheme_post_video( $post_id = null, $args = array(), $query_args = array() ) {
+	global $wp_embed;
+	
 	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 	$video_url = get_audiotheme_post_video_url( $post_id );
 	
-	$args = wp_parse_args( $args, array(
-		'discover' => apply_filters( 'embed_oembed_discover', false ) && author_can( $post_id, 'unfiltered_html' )
-	) );
-	
 	$html = '';
 	if ( $video_url ) {
-		$html = wp_oembed_get( add_query_arg( $query_args, $video_url ), $args );
+		// save current embed settings and restore them after running shortcode
+		$restore_post_ID = $wp_embed->post_ID;
+		$restore_linkifunknown = $wp_embed->linkifunknown;
+		$restore_usecache = $wp_embed->usecache;
+		
+		// can't be sure what the embed settings are, so explicitly set them
+		$wp_embed->post_ID = $post_id; // allows WP_Embed caching to work when this function is called outside of the loop
+		$wp_embed->linkifunknown = false;
+		$wp_embed->usecache = true;
+		
+		$html = $wp_embed->shortcode( $args, add_query_arg( $query_args, $video_url ) );
+		
+		// restore global embed settings
+		$wp_embed->post_ID = $restore_post_ID;
+		$wp_embed->linkifunknown = $restore_linkifunknown;
+		$wp_embed->usecache = $restore_usecache;
 	}
 	
 	return apply_filters( 'audiotheme_post_video_html', $html, $post_id, $video_url, $args, $query_args );
