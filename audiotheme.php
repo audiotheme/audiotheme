cc709dcdbade43278e2cbbdceab6d5f44914b18b
Brady Vercher <brady@blazersix.com>
2013-04-16 20:56:11 -0700

    Introduce an API to enqueue tracks and playlists.

--- audiotheme.php
+++ audiotheme.php
@@ -97,6 +97,7 @@ function audiotheme_load() {
 	add_filter( 'embed_oembed_html', 'audiotheme_oembed_html', 10, 4 );
 	add_filter( 'embed_handler_html', 'audiotheme_oembed_html', 10, 4 );
 	add_filter( 'video_embed_html', 'audiotheme_oembed_html', 10 ); // Jetpack compat.
+	add_action( 'wp_print_footer_scripts', 'audiotheme_print_tracks_js' );
 
 	// Archive hooks.
 	add_action( 'init', 'register_audiotheme_archives' );
