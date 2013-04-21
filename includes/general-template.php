4a606640f7b704683c14079704251e4cc5c6e5e8
Brady Vercher <brady@blazersix.com>
2013-04-20 22:20:56 -0700

    Introduce audiotheme_locate_template() for loading default templates from the plugin directory.

--- includes/general-template.php
+++ includes/general-template.php
@@ -7,6 +7,48 @@
  */
 
 /**
+ * Retrieve the name of the highest priority template file that exists.
+ *
+ * Searches in the STYLESHEETPATH before TEMPLATEPATH so that themes which
+ * inherit from a parent theme can just overload one file. Falls back to
+ * the built-in template.
+ *
+ * @since 1.1.0
+ * @see locate_template()
+ *
+ * @param string|array $template_names Template file(s) to search for, in order.
+ * @param bool $load If true the template file will be loaded if it is found.
+ * @param bool $require_once Whether to require_once or require. Default true. Has no effect if $load is false.
+ * @return string The template path if one is located.
+ */
+function audiotheme_locate_template( $template_names, $load = false, $require_once = true ) {
+	$template = '';
+
+	foreach ( (array) $template_names as $template_name ) {
+		if ( ! $template_name ) {
+			continue;
+		}
+
+		if ( file_exists( get_stylesheet_directory() . '/audiotheme/' . $template_name ) ) {
+			$template = get_stylesheet_directory() . '/audiotheme/' . $template_name;
+			break;
+		} elseif ( file_exists( get_template_directory() . '/audiotheme/' . $template_name ) ) {
+			$template = get_template_directory() . '/audiotheme/' . $template_name;
+			break;
+		} elseif ( file_exists( AUDIOTHEME_DIR . 'templates/' . $template_name ) ) {
+			$template = AUDIOTHEME_DIR . 'templates/' . $template_name;
+			break;
+		}
+	}
+
+	if ( $load && ! empty( $template ) ) {
+		load_template( $template, $require_once );
+	}
+
+	return $template;
+}
+
+/**
  * Display a post type archive title.
  *
  * Just a wrapper to the default post_type_archive_title for the sake of
