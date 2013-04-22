37d4a77c72091757200ac0727f793389133f9c8d
Brady Vercher <brady@blazersix.com>
2013-04-21 18:15:19 -0700

    Remove source files for external scripts.

--- audiotheme.php
+++ audiotheme.php
@@ -159,17 +159,16 @@ add_action( 'after_setup_theme', 'audiotheme_load_admin', 5 );
 function audiotheme_register_scripts() {
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-	wp_register_script( 'jquery-fitvids', AUDIOTHEME_URI . 'includes/js/jquery.fitvids' . $suffix . '.js', array( 'jquery' ), '1.0', true );
-	wp_register_script( 'jquery-jplayer', AUDIOTHEME_URI . 'includes/js/jquery.jplayer' . $suffix . '.js', array( 'jquery' ), '2.2.19', true );
-	wp_register_script( 'jquery-jplayer-playlist', AUDIOTHEME_URI . 'includes/js/jquery.jplayer.playlist' . $suffix . '.js', array( 'jquery-jplayer' ), '2.2.2', true );
-	wp_register_script( 'jquery-placeholder', AUDIOTHEME_URI . 'includes/js/jquery.placeholder' . $suffix . '.js', array( 'jquery' ), '2.0.7', true );
-	wp_register_script( 'jquery-timepicker', AUDIOTHEME_URI . 'includes/js/jquery.timepicker' . $suffix . '.js', array( 'jquery' ), '1.1', true );
+	wp_register_script( 'jquery-fitvids', AUDIOTHEME_URI . 'includes/js/jquery.fitvids.min.js', array( 'jquery' ), '1.0', true );
+	wp_register_script( 'jquery-jplayer', AUDIOTHEME_URI . 'includes/js/jquery.jplayer.min.js', array( 'jquery' ), '2.2.19', true );
+	wp_register_script( 'jquery-jplayer-playlist', AUDIOTHEME_URI . 'includes/js/jquery.jplayer.playlist.min.js', array( 'jquery-jplayer' ), '2.2.2', true );
+	wp_register_script( 'jquery-placeholder', AUDIOTHEME_URI . 'includes/js/jquery.placeholder.min.js', array( 'jquery' ), '2.0.7', true );
+	wp_register_script( 'jquery-timepicker', AUDIOTHEME_URI . 'includes/js/jquery.timepicker.min.js', array( 'jquery' ), '1.1', true );
 
 	wp_localize_script( 'jquery-jplayer', 'AudiothemeJplayer', array(
 		'swfPath' => AUDIOTHEME_URI . 'includes/js'
 	) );
 }
-
 
 /**
 * Support localization for the plugin strings.
