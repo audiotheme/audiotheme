bf44530d0cdd86dfb69eeb3adf0f7e34a6bc3871
Luke McDonald <thelukemcdonald@gmail.com>
2013-12-17 14:17:10 -0600

    Set Record default template posts_per_archive_page value if it hasn't been set

--- discography/discography.php
+++ discography/discography.php
@@ -296,9 +296,9 @@ function audiotheme_record_default_template_query( $query ) {
 	}
 
 	if ( is_audiotheme_default_template( audiotheme_locate_template( 'archive-record.php' ) ) ) {
-		$posts_per_page = ( $posts_per_archive_page = get_query_var( 'posts_per_archive_page' ) ) ? $posts_per_archive_page : 12;
-
-		$query->set( 'posts_per_archive_page', $posts_per_page );
+		if ( '' == $query->get( 'posts_per_archive_page' ) ) {
+			$query->set( 'posts_per_archive_page', 12 );
+		}
 	}
 }
 
