7dd0f14b24844d2e22edd209232fe8866760a361
Luke McDonald <thelukemcdonald@gmail.com>
2014-03-19 14:05:33 -0500

    Add classes to Recent Posts widget markup.

--- templates/widgets/recent-posts.php
+++ templates/widgets/recent-posts.php
@@ -14,7 +14,10 @@ if ( ! empty( $title ) ) :
 		echo $title;
 
 		if ( $show_feed_link ) :
-			echo '<a href="' . esc_url( $feed_link ) . '">' . __( 'Feed', 'audiotheme' ) . '</a>';
+			printf( '<a class="recent-posts-feed-link" href="%s">%s</a>',
+				esc_url( $feed_link ),
+				__( 'Feed', 'audiotheme' )
+			);
 		endif;
 
 	echo $after_title;
@@ -22,15 +25,15 @@ endif;
 ?>
 
 <?php if ( $loop->have_posts() ) : ?>
-	<ul>
+	<ul class="recent-posts-list">
 
 	<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-		<li>
-			<?php the_title( '<h5><a href="' . esc_url( get_permalink() ) . '">', '</a></h5>' ); ?>
+		<li class="recent-posts-item">
+			<?php the_title( '<h5 class="recent-posts-item-title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h5>' ); ?>
 
 			<?php
 			if ( $show_date ) :
-				$date_html = sprintf( '<time datetime="%s" pubdate="pubdate" class="published">%s</time>',
+				$date_html = sprintf( '<time class="recent-posts-item-date" datetime="%s" pubdate="pubdate" class="published">%s</time>',
 					get_post_time( 'c', true ),
 					get_the_time( $date_format )
 				);
@@ -42,7 +45,9 @@ endif;
 			<?php
 			if ( $show_excerpts ) :
 				$excerpt = wpautop( wp_html_excerpt( get_the_excerpt(), $excerpt_length, '&hellip;' ) );
-				echo apply_filters( 'audiotheme_widget_recent_posts_excerpt', $excerpt, $loop->post, $instance );
+				printf( '<div class="recent-posts-item-excerpt">%s</div>',
+					apply_filters( 'audiotheme_widget_recent_posts_excerpt', $excerpt, $loop->post, $instance )
+				);
 			endif;
 			?>
 		</li>
