d79f3da0cd39faa2856e36300379a8f28064040c
Brady Vercher <brady@blazersix.com>
2015-05-12 17:56:44 -0700

    Replace deprecated call to like_escape().

--- modules/gigs/admin/ajax.php
+++ modules/gigs/admin/ajax.php
@@ -7,8 +7,9 @@
 function audiotheme_ajax_get_venue_matches() {
 	global $wpdb;
 
-	$var = like_escape( stripslashes( $_GET['term'] ) ) . '%';
-	$venues = $wpdb->get_col( $wpdb->prepare( "SELECT post_title FROM $wpdb->posts WHERE post_type='audiotheme_venue' AND post_title LIKE %s ORDER BY post_title ASC", $var ) );
+	$var    = $wpdb->esc_like( stripslashes( $_GET['term'] ) ) . '%';
+	$sql    = $wpdb->prepare( "SELECT post_title FROM $wpdb->posts WHERE post_type='audiotheme_venue' AND post_title LIKE %s ORDER BY post_title ASC", $var );
+	$venues = $wpdb->get_col( $sql );
 
 	wp_send_json( $venues );
 }
@@ -21,7 +22,8 @@ function audiotheme_ajax_get_venue_matches() {
 function audiotheme_ajax_is_new_venue() {
 	global $wpdb;
 
-	$venue = $wpdb->get_col( $wpdb->prepare( "SELECT post_title FROM $wpdb->posts WHERE post_type='audiotheme_venue' AND post_title=%s ORDER BY post_title ASC LIMIT 1", stripslashes( $_GET['name'] ) ) );
+	$sql   = $wpdb->prepare( "SELECT post_title FROM $wpdb->posts WHERE post_type='audiotheme_venue' AND post_title=%s ORDER BY post_title ASC LIMIT 1", stripslashes( $_GET['name'] ) );
+	$venue = $wpdb->get_col( $sql );
 
 	wp_send_json( $venue );
 }
