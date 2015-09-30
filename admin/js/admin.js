68e73b337b77f5f07dc86c5bf620588ac3030cb5
Brady Vercher <brady@blazersix.com>
2015-09-30 08:37:59 -0700

    Remove code for the old media popup (before WP 3.5).

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,5 +1,4 @@
 /*jshint browserify:true */
-/*global tb_remove:false */
 
 var wp = require( 'wp' );
 
@@ -8,70 +7,6 @@ jQuery(function($) {
 		$(this).parent().addClass('is-focused');
 	}).on('blur', '.audiotheme-input-group input', function() {
 		$(this).parent().removeClass('is-focused');
-	});
-});
-
-/**
- * Media Popup Helper
- *
- * Monitors clicks on any element with a class of "thickbox" and checks
- * for data attributes to modify the behavior the media popup.
- *
- * To change the "Insert Into Post" button text, add the following attribute
- * to the calling element: data-insert-button-text="New Text"
- *
- * To change the field that the media popup inserts the url to the selected
- * media item, add the following attribute with the id of the target element
- * as its value: data-insert-field="Target ID"
- */
-jQuery(function($) {
-	var audiothemeInsertField = null,
-		audiothemeInsertButtonText = null,
-		audiothemeInsertButtonInterval = null;
-
-	$('body').on('click', '.thickbox', function() {
-		var $this = $(this),
-			insertField = $this.data('insert-field'),
-			insertButtonText = $this.data('insert-button-text');
-
-		clearInterval(audiothemeInsertButtonInterval);
-
-		if ( 'undefined' !== typeof insertField ) {
-			audiothemeInsertField = insertField;
-		}
-
-		if ( 'undefined' !== typeof insertButtonText ) {
-			audiothemeInsertButtonText = insertButtonText;
-
-			audiothemeInsertButtonInterval = setInterval( function() {
-				var buttons = $('#TB_iframeContent').contents().find('.button[name^="send"], #insertonlybutton');
-
-				buttons.val( audiothemeInsertButtonText );
-
-				if (audiothemeInsertField.length) {
-					buttons.off('click').on('click.audiotheme', function(e) {
-						var $this = $(this),
-							mediaItem = $this.closest('table'),
-							url;
-
-						e.preventDefault();
-
-						if ( mediaItem.find('#src').length ) {
-							url = mediaItem.find('#src').val();
-						} else if ( mediaItem.find('.urlfile').length ) {
-							url = $(this).closest('table').find('.urlfile').data('link-url');
-						}
-
-						jQuery('#' + audiothemeInsertField).val(url);
-						tb_remove();
-
-						audiothemeInsertField = null;
-						audiothemeInsertButtonText = null;
-						clearInterval(audiothemeInsertButtonInterval);
-					});
-				}
-			}, 500 );
-		}
 	});
 });
 
