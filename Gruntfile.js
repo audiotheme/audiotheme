a92b4bf3986004b3026ed150f26dfd560070b618
Brady Vercher <brady@blazersix.com>
2015-11-17 10:04:54 -0800

    Remove P2P and plugin header strings from the POT file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -172,13 +172,33 @@ module.exports = function( grunt ) {
 		makepot: {
 			build: {
 				options: {
-					exclude: [ '.git/.*', 'dist/.*', 'node_modules/.*', 'release/.*', 'tests/.*' ],
+					exclude: [ '.git/.*', 'dist/.*', 'includes/vendor/.*', 'node_modules/.*', 'release/.*', 'tests/.*' ],
 					mainFile: 'audiotheme.php',
 					potHeaders: {
 						'report-msgid-bugs-to': 'https://audiotheme.com/support/',
 						'poedit': true
 					},
-					type: 'wp-plugin'
+					type: 'wp-plugin',
+					processPot: function( pot ) {
+						var translation,
+							excludedMeta = [
+								'Plugin Name of the plugin/theme',
+								'Plugin URI of the plugin/theme',
+								'Author of the plugin/theme',
+								'Author URI of the plugin/theme'
+							];
+
+						for ( translation in pot.translations[''] ) {
+							if ( 'undefined' !== typeof pot.translations[''][ translation ].comments.extracted ) {
+								if ( 0 <= excludedMeta.indexOf( pot.translations[''][ translation ].comments.extracted ) ) {
+									console.log( 'Excluded meta: ' + pot.translations[''][ translation ].comments.extracted );
+									delete pot.translations[''][ translation ];
+								}
+							}
+						}
+
+						return pot;
+					}
 				}
 			}
 		},
