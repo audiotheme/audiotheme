ca4a289bf02e7b048f116d111678bd1f4ac8e614
Brady Vercher <brady@blazersix.com>
2016-09-15 18:47:39 -0700

    Use a div instead of h1 for the dashboard logo to prevent general notices from botching the header.

--- admin/views/screen-dashboard-header.php
+++ admin/views/screen-dashboard-header.php
@@ -14,9 +14,9 @@
 
 <header class="audiotheme-dashboard-hero">
 	<div class="audiotheme-dashboard-hero-branding">
-		<h1 class="audiotheme-dashboard-hero-title"><a href="https://audiotheme.com/view/audiotheme/" target="_blank"><svg viewBox="0 0 80 40">
+		<div class="audiotheme-dashboard-hero-title"><a href="https://audiotheme.com/view/audiotheme/" target="_blank"><svg viewBox="0 0 80 40">
 			<path fill="#E4002B" d="M10,40C4.5,40,0,35.5,0,30s4.5-10,10-10s10,4.5,10,10S15.5,40,10,40z M0,0l40,40V20L20,0H0z M80,0H40v14h40V0z M74,40L60,18L46,40H74z"/>
-		</svg></a></h1>
+		</svg></a></div>
 		<p>
 			<?php esc_html_e( 'AudioTheme has the tools you need to easily manage your gigs, discography, videos and more.', 'audiotheme' ); ?>
 		</p>
