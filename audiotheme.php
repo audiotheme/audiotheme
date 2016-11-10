584e536c5c7d82de2e8c4db656b5e181058ed5d8
Brady Vercher <brady@blazersix.com>
2016-11-10 09:06:58 -0800

    Release 2.0.0.

--- audiotheme.php
+++ audiotheme.php
@@ -10,7 +10,7 @@
  * Plugin Name: AudioTheme
  * Plugin URI:  https://audiotheme.com/view/audiotheme/
  * Description: A platform for music-oriented websites, allowing for easy management of gigs, discography, videos and more.
- * Version:     2.0.0-beta
+ * Version:     2.0.0
  * Author:      AudioTheme
  * Author URI:  https://audiotheme.com/
  * License:     GPL-2.0+
@@ -42,7 +42,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * The plugin version.
  */
-define( 'AUDIOTHEME_VERSION', '2.0.0-beta' );
+define( 'AUDIOTHEME_VERSION', '2.0.0' );
 
 /**
  * Plugin path.
