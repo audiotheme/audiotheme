830c21a867e579438d62afb877a67bf42c44bd57
Brady Vercher <brady@blazersix.com>
2017-11-08 14:08:28 -0800

    Update vendor scripts.

--- classes/Provider/Assets.php
+++ classes/Provider/Assets.php
@@ -38,13 +38,13 @@ class AudioTheme_Provider_Assets extends AudioTheme_AbstractProvider {
 		$suffix   = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
 		wp_register_script( 'audiotheme',              $base_url  .'/audiotheme' . $suffix . '.js',     array( 'jquery', 'jquery-jplayer', 'jquery-fitvids' ), '1.0.0',  true );
-		wp_register_script( 'jquery-fitvids',          $base_url  .'/vendor/jquery.fitvids.min.js',     array( 'jquery' ),                                     '1.1.0',  true );
+		wp_register_script( 'jquery-fitvids',          $base_url  .'/vendor/jquery.fitvids.min.js',     array( 'jquery' ),                                     '1.2.0',  true );
 		wp_register_script( 'jquery-jplayer',          $base_url  .'/vendor/jquery.jplayer.min.js',     array( 'jquery' ),                                     '2.9.2', true );
 		wp_register_script( 'jquery-jplayer-playlist', $base_url  .'/vendor/jplayer.playlist.min.js',   array( 'jquery-jplayer' ),                             '2.9.2',  true );
-		wp_register_script( 'jquery-placeholder',      $base_url  .'/vendor/jquery.placeholder.min.js', array( 'jquery' ),                                     '2.0.7',  true );
-		wp_register_script( 'jquery-timepicker',       $base_url  .'/vendor/jquery.timepicker.min.js',  array( 'jquery' ),                                     '1.6.11', true );
-		wp_register_script( 'moment',                  $base_url  .'/vendor/moment.min.js',             array(),                                               '2.10.6', true );
-		wp_register_script( 'pikaday',                 $base_url  .'/vendor/pikaday.min.js',            array( 'moment'),                                      '1.4.0',  true );
+		wp_register_script( 'jquery-placeholder',      $base_url  .'/vendor/jquery.placeholder.min.js', array( 'jquery' ),                                     '2.3.1',  true );
+		wp_register_script( 'jquery-timepicker',       $base_url  .'/vendor/jquery.timepicker.min.js',  array( 'jquery' ),                                     '1.11.12', true );
+		wp_register_script( 'moment',                  $base_url  .'/vendor/moment.min.js',             array(),                                               '2.19.1', true );
+		wp_register_script( 'pikaday',                 $base_url  .'/vendor/pikaday.min.js',            array( 'moment'),                                      '1.6.1',  true );
 
 		wp_localize_script( 'jquery-jplayer', 'AudiothemeJplayer', array(
 			'swfPath' => $base_url . '/vendor',
