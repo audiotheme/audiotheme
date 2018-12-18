6835e5c47b80e266b69040ba37859c5cedfebf02
Brady Vercher <brady@blazersix.com>
2018-12-18 10:29:42 -0800

    Update Pikaday and Moment.js vendor scripts.
    WordPress 5.0 includes Moment.js, so the bundled version is only loaded if it
    hasn't already been registered.

--- classes/Provider/Assets.php
+++ classes/Provider/Assets.php
@@ -37,14 +37,17 @@ class AudioTheme_Provider_Assets extends AudioTheme_AbstractProvider {
 		$base_url = set_url_scheme( $this->plugin->get_url( 'includes/js' ) );
 		$suffix   = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-		wp_register_script( 'audiotheme',              $base_url  .'/audiotheme' . $suffix . '.js',     array( 'jquery', 'jquery-jplayer', 'jquery-fitvids' ), '1.0.0',  true );
-		wp_register_script( 'jquery-fitvids',          $base_url  .'/vendor/jquery.fitvids.min.js',     array( 'jquery' ),                                     '1.2.0',  true );
-		wp_register_script( 'jquery-jplayer',          $base_url  .'/vendor/jquery.jplayer.min.js',     array( 'jquery' ),                                     '2.9.2', true );
-		wp_register_script( 'jquery-jplayer-playlist', $base_url  .'/vendor/jplayer.playlist.min.js',   array( 'jquery-jplayer' ),                             '2.9.2',  true );
-		wp_register_script( 'jquery-placeholder',      $base_url  .'/vendor/jquery.placeholder.min.js', array( 'jquery' ),                                     '2.3.1',  true );
-		wp_register_script( 'jquery-timepicker',       $base_url  .'/vendor/jquery.timepicker.min.js',  array( 'jquery' ),                                     '1.11.12', true );
-		wp_register_script( 'moment',                  $base_url  .'/vendor/moment.min.js',             array(),                                               '2.19.1', true );
-		wp_register_script( 'pikaday',                 $base_url  .'/vendor/pikaday.min.js',            array( 'moment'),                                      '1.6.1',  true );
+		if ( ! wp_script_is( 'moment', 'registered' ) ) {
+			wp_register_script( 'moment', $base_url .'/vendor/moment.min.js', array(), '2.23.0', true );
+		}
+
+		wp_register_script( 'audiotheme',              $base_url .'/audiotheme' . $suffix . '.js',     array( 'jquery', 'jquery-jplayer', 'jquery-fitvids' ), '1.0.0',  true );
+		wp_register_script( 'jquery-fitvids',          $base_url .'/vendor/jquery.fitvids.min.js',     array( 'jquery' ),                                     '1.2.0',  true );
+		wp_register_script( 'jquery-jplayer',          $base_url .'/vendor/jquery.jplayer.min.js',     array( 'jquery' ),                                     '2.9.2', true );
+		wp_register_script( 'jquery-jplayer-playlist', $base_url .'/vendor/jplayer.playlist.min.js',   array( 'jquery-jplayer' ),                             '2.9.2',  true );
+		wp_register_script( 'jquery-placeholder',      $base_url .'/vendor/jquery.placeholder.min.js', array( 'jquery' ),                                     '2.3.1',  true );
+		wp_register_script( 'jquery-timepicker',       $base_url .'/vendor/jquery.timepicker.min.js',  array( 'jquery' ),                                     '1.11.12', true );
+		wp_register_script( 'pikaday',                 $base_url .'/vendor/pikaday.min.js',            array( 'moment'),                                      '1.8.0',  true );
 
 		wp_localize_script( 'jquery-jplayer', 'AudiothemeJplayer', array(
 			'swfPath' => $base_url . '/vendor',
